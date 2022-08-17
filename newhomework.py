184184b6062c00b815d37ad298e43857a3097555
kripesh poudel <pkripesh345@gmail.com>
2022-08-17 19:11:54 +0545

    August 16th Homework

---
+++ newhomework.py
@@ -0,0 +1,8 @@
+# to prinit name age and address
+def personal_details():
+    name, age = "Kripesh", 18
+    address = "Lokanthali, Bhaktapur"
+    print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
+
+personal_details()
+
